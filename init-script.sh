713d21c9116dc2eef2ea7eab6bbcb036bbba2788
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2025-04-22 19:34:56 +0530

    Update init-script.sh

--- init-script.sh
+++ init-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-echo "Hello Terraform!" > test.txt
+
 sudo -s
 apt-get update
 apt-get install -y apache2
@@ -10,3 +10,6 @@ systemctl enable apache2
 git clone https://github.com/SmithaVerity/ABTestingApp.git
 
 mv ABTestingApp/cafe /var/www/html
+
+git clone https://github.com/SmithaVerity/ABTestingApp.git
+echo "Hello Terraform!" > test.txt
