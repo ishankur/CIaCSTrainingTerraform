3b1f99d6521e3105f5e38972c2045d72f9afa2fc
ishankur <105223155+ishankur@users.noreply.github.com>
2025-05-15 13:35:39 +0530

    Update init-script.sh

--- init-script.sh
+++ init-script.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-sudo -s
-apt-get update
-apt-get install -y apache2
-systemctl enable apache2
+sudo apt-get update
+sudo apt-get install -y apache2
+sudo systemctl enable apache2
+sudo systemctl start apache2
 
 git clone https://github.com/SmithaVerity/ABTestingApp.git
 
-mv ABTestingApp/cafe /var/www/html
-
+sudo mv ABTestingApp/cafe /var/www/html
+sudo chmod -R 755 /var/www/html/cafe
